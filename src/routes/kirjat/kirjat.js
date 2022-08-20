25a9134e6df7f6f3133d0a20d6f5a9c0114fd7e8
lainevesku <lainevesku@gmail.com>
2022-08-20 14:40:16 +0300

    Kirjat komponentti

---
+++ src/routes/kirjat/kirjat.js
@@ -0,0 +1,15 @@
+import Kirja from "../../components/kirja/kirja";
+
+function Kirjat(props) {
+    return(
+        <div>
+            <Kirja />
+            <Kirja />
+            <Kirja />
+            <Kirja />
+        </div>
+
+    );
+}
+
+export default Kirjat;
