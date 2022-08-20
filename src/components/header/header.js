cd87465f09bbe0e96f56243f7ff4613f9438d604
lainevesku <lainevesku@gmail.com>
2022-08-20 14:38:33 +0300

    Header komponentti

---
+++ src/components/header/header.js
@@ -0,0 +1,11 @@
+import styles from './header.module.scss';
+
+function Header() {
+    return(
+        <div className={styles.header}>
+            Fannyn kirjasovellus
+        </div>
+    );
+}
+
+export default Header;
