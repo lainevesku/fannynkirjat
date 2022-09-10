03344f9bfcf2143dfa03ab6037bbd0606a213137
lainevesku <lainevesku@gmail.com>
2022-09-11 00:25:04 +0300

    Laitettu kommentteihin sijainti lista luomista

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -21,19 +21,32 @@ import { ButtonAppContainer } from '../../shared/uibuttons/uibuttons';
 function App() {
 
   const [data, setData] = useState([]);
+  // const [sijaintilist, setSijaintilist] = useState([]); // Jos tulevaisuudessa sijaintiin omia lisäys mahdollisuuksia 1
   
 
   const user = useUser();
 
   const kirjaCollectionRef = useFirestore().collection('user').doc(user.data.uid).collection('kirja');
   const { data: kirjaCollection } = useFirestoreCollectionData(kirjaCollectionRef.orderBy("kirjailija"), {initialData: [], idField: "id"}) ;
+
   const arvosteluCollectionRef = useFirestore().collection('user').doc(user.data.uid).collection('kirja');
   const { data: arvosteluCollection }  = useFirestoreCollectionData(arvosteluCollectionRef.orderBy("rating_koko", "desc"), {initialData: [], idField: "id"});
 
+  /* Jos tulevaisuudessa sijaintiin omia lisäys mahdollisuuksia 2
+  const sijaintiCollectionRef = useFirestore().collection('user').doc(user.data.uid).collection('sijainti');
+  const { data: sijaintiCollection } = useFirestoreCollectionData(sijaintiCollectionRef.orderBy("sijainti"), {initialData: []});
+  */
 
   useEffect(() => {
     setData(kirjaCollection);
   }, [kirjaCollection]);
+
+  /* Jos tulevaisuudessa sijaintiin omia lisäys mahdollisuuksia 3
+  useEffect(() => {
+    const sijainti = sijaintiCollection.map(obj => obj.sijainti);
+    setSijaintilist(sijainti);
+  }, [sijaintiCollection]);
+  */
 
   const handleKirjaSubmit = (newkirja) => {
    kirjaCollectionRef.doc(newkirja.id).set(newkirja);
@@ -42,6 +55,12 @@ function App() {
   const handleKirjaDelete = (id) => {
     kirjaCollectionRef.doc(id).delete();
   }
+
+  /* Jos tulevaisuudessa sijaintiin omia lisäys mahdollisuuksia 4
+  const handleSijaintiSubmit = (newsijainti) => {
+    sijaintiCollectionRef.doc().set({sijainti: newsijainti});
+  }
+  */
 
   return (
     <ButtonAppContainer>
@@ -56,13 +75,13 @@ function App() {
               <Arvostelut data={arvosteluCollection}/>
             </Route>
             <Route path="/settings">
-              <Settings />
+              <Settings /*sijainti={sijaintilist} onSijaintiSubmit={handleSijaintiSubmit} Jos tulevaisuudessa sijaintiin omia lisäys mahdollisuuksia 5 */ />
             </Route>
             <Route path="/add">
-              <AddKirja onKirjaSubmit={handleKirjaSubmit} />
+              <AddKirja onKirjaSubmit={handleKirjaSubmit} /*sijainti={sijaintilist} Jos tulevaisuudessa sijaintiin omia lisäys mahdollisuuksia 6 */ />
             </Route>
             <Route path="/edit/:id">
-              <EditKirja onKirjaSubmit={handleKirjaSubmit} data={data} onKirjaDelete={handleKirjaDelete} />
+              <EditKirja onKirjaSubmit={handleKirjaSubmit} data={data} /*sijainti={sijaintilist} Jos tulevaisuudessa sijaintiin omia lisäys mahdollisuuksia 7*/ onKirjaDelete={handleKirjaDelete} />
             </Route>
             <Route path="/info/:id">
               <InfoKirja data={data} />
--- src/routes/settings/settings.js
+++ src/routes/settings/settings.js
@@ -1,8 +1,9 @@
+import styles from './settings.module.scss';
 import { useUser, useAuth } from "reactfire";
 import Button from "../../shared/uibuttons/uibuttons";
 import { useHistory } from "react-router-dom";
 
-function Settings(props) {
+function Settings() {
 
     const user = useUser();
     const auth = useAuth();
@@ -13,9 +14,16 @@ function Settings(props) {
         history.push('.');
         window.location.reload();
     }
-
+/*  Jos tulevaisuudessa sijaintiin omia lisäys mahdollisuuksia 1
+    const handleSijaintiSubmit = (event) => {
+        event.preventDefault();
+        const newsijainti = event.target.elements.sijainti.value;
+        props.onSijaintiSubmit(newsijainti);
+        event.target.elements.sijainti.value = "";
+    }
+*/
     return(
-        <>
+        <div className={styles.settings}>
             <h2>Asetukset</h2>
             <div>
                 <div>
@@ -26,7 +34,18 @@ function Settings(props) {
                 </div>
                 <Button secondary onClick={signOut}>Kirjaudu ulos</Button>
             </div>
-        </>
+           {/* Jos tulevaisuudessa sijaintiin omia lisäys mahdollisuuksia 2
+           <h3>Sijainti lista</h3>
+            <div>
+                { props.sijainti.map((sijainti) => <div key={sijainti}>{sijainti}</div> )}
+                <form onSubmit={handleSijaintiSubmit}>
+                    <div>
+                        <input type="text" name="sijainti" />
+                        <Button type="submit" primary>LISÄÄ</Button>
+                    </div>
+                </form>
+    </div>*/}
+        </div>
     );
 }
 
